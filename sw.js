c8ebf759fd54406ed8de10066eb6b26cae007ace
527wdsfc58-web <527wdsfc58@privaterelay.appleid.com>
2026-09-10 21:14:01 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='forge-v0.15';
+const CACHE='forge-v0.16';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('forge-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
